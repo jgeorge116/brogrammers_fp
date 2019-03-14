a08cca1489a47cdc83f3d489f2c99b5f0a925b15
Joel George <joelgeorge556@gmail.com>
2019-03-14 14:00:00 -0400

    finally able to connect to remote mongo database

--- back-end/server.js
+++ back-end/server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://130.245.171.174:27017/test', { useNewUrlParser: true })
+MongoClient.connect('mongodb://130.245.171.174:27017/stackOverflowDB', { useNewUrlParser: true })
   .then((db) => console.log("YEEEEEE, CONNECTED!!")).catch((err) => console.log("NOOOOOO",err))
-
-  /*
-    * made db on an open stack instance
-    * connection doesn't work yet, even tho i added the the rule, enabled mongo on the firewall, and changed the config file
-    * ip is: 130.245.171.174
-    * I think the string isn't formatted correctly cuz for remote dbs, it's formatted like..
-    * mongodb://[username]:[password]@[host]:[port]/[database]
-    * too lazy to fix now lmaoo
-  */
+  
