d0532ae30448a422be42b48b8e4eb5a6692884f8
swuleung <sammiwuleung@gmail.com>
2019-03-19 12:42:31 -0400

    Added back index.js

---
+++ front-end/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Components/style/index.css";
+import App from "./Components/App";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import verify from "./Components/verifyUser";
+import home from "./Components/home";
+import login from "./Components/login";
+import logout from "./Components/logout";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/adduser" component={App} />
+      <Route path="/verify" component={verify} />
+      <Route path="/home" component={home} />
+      <Route path="/login" component={login} />
+      <Route path="/logout" component={logout} />
+      <Redirect from="/" exact to="/adduser" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
