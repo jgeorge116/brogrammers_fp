31d757800726cd5a2aa8ba63aaa26c8514cc75ca
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-03-19 22:21:09 -0400

    Finished checking questions

--- back-end/repositories/questionRepository.js
+++ back-end/repositories/questionRepository.js
@@ -17,16 +17,28 @@ module.exports = class QuestionRepository {
   async create(username, title, body, tags) {
     // Check the fields
     if (!username) {
-      return { status: "error", data: "Username is required" };
+      return {
+        status: "error",
+        data: "Username is required"
+      };
     }
     if (!title) {
-      return { status: "error", data: "Title is required" };
+      return {
+        status: "error",
+        data: "Title is required"
+      };
     }
     if (!body) {
-      return { status: "error", data: "Body is required" };
+      return {
+        status: "error",
+        data: "Body is required"
+      };
     }
     if (!tags) {
-      return { status: "error", data: "Tags are required" };
+      return {
+        status: "error",
+        data: "Tags are required"
+      };
     }
     const new_id = uuidv4();
     const new_question = new QuestionModel({
@@ -37,7 +49,10 @@ module.exports = class QuestionRepository {
       tags: tags
     });
     await new_question.save();
-    return { status: "OK", data: new_id };
+    return {
+      status: "OK",
+      data: new_id
+    };
   }
 
   /**
@@ -45,12 +60,20 @@ module.exports = class QuestionRepository {
    * @param {String} id 
    */
   async get_questions_by_id(id) {
-    var found_question = await QuestionModel.findOne({ id: id });
+    var found_question = await QuestionModel.findOne({
+      id: id
+    });
     if (!found_question) {
-      return { status: "error", data: "Question does not exist" };
+      return {
+        status: "error",
+        data: "Question does not exist"
+      };
     }
     var question = await this.question_to_api_format(found_question);
-    return { status: question.status, data: question.data };
+    return {
+      status: question.status,
+      data: question.data
+    };
   }
 
   /**
@@ -59,14 +82,18 @@ module.exports = class QuestionRepository {
    * @param {String} info 
    */
   async add_view_to_question(id, info) {
-    var found_question = await QuestionModel.findOne({ id: id });
+    var found_question = await QuestionModel.findOne({
+      id: id
+    });
     if (!found_question) {
       // Don't add a view if question does not exist
       return;
     }
     if (info.type == "IP") {
       // Don't increment if there is a view with this IP
-      const find_view = await ViewQuestionModel.findOne({ ip: info.query });
+      const find_view = await ViewQuestionModel.findOne({
+        ip: info.query
+      });
       if (find_view) {
         return;
       }
@@ -91,25 +118,68 @@ module.exports = class QuestionRepository {
     }
   }
 
+  /**
+   * Searches for questions based on timestamp, limit, accepted (only questions with accepted answers)
+   * @param {Integer} timestamp - Optional
+   * @param {Integer} limit - Optional
+   * @param {boolean} accepted - Optional
+   */
   async search_questions(timestamp, limit, accepted) {
     var search_timestamp = timestamp;
-    if (!search_timestamp) search_timestamp = new Date().getTime();
+    if (!search_timestamp) {
+      search_timestamp = new Date().getTime();
+    }
+    if (!Number.isInteger(search_timestamp) || search_timestamp < 0) {
+      return {
+        status: "error",
+        data: "Timestamp has to be a non-negative integer"
+      };
+    }
+    // Search limit defaults to 25, and maxes out at 100
     var search_limit = limit;
-    if (!search_limit) search_limit = 25;
-    if (search_limit > 100) search_limit = 100;
+    if (!search_limit) {
+      search_limit = 25;
+    }
+    if (!Number.isInteger(search_limit) || search_limit < 1) {
+      return {
+        status: "error",
+        data: "Limit has to be a positive integer"
+      };
+    }
+    if (search_limit > 100) {
+      search_limit = 100;
+    }
     var search_accepted = accepted;
-    if (!search_accepted) search_accepted = false;
+    if (!search_accepted) {
+      search_accepted = false;
+    }
+    if (typeof search_accepted !== 'boolean') {
+      return {
+        status: "error",
+        data: "Accepted has to be a boolean"
+      };
+    }
     var search_results;
     if (search_accepted) {
       search_results = await QuestionModel.find({
-        timestamp: { $lte: search_timestamp },
-        accepted_answer_id: { $ne: null }
-      });
+          timestamp: {
+            $lte: search_timestamp
+          },
+          accepted_answer_id: {
+            $ne: null
+          }
+        })
+        .limit(search_limit)
+        .sort({
+          timestamp: -1
+        });
     } else {
-      var search_results = await QuestionModel.find({
-        timestamp: { $lte: search_timestamp },
-        accepted_answer_id: null
-      })
+      search_results = await QuestionModel.find({
+          timestamp: {
+            $lte: search_timestamp
+          },
+          accepted_answer_id: null
+        })
         .limit(search_limit)
         .sort({
           timestamp: -1
@@ -121,21 +191,32 @@ module.exports = class QuestionRepository {
         search_results[result]
       );
       if (question_info.status == "error")
-        return { status: "error", data: "Error fetching question data" };
+        return {
+          status: "error",
+          data: "Error fetching question data"
+        };
       all_questions.push(question_info.data);
     }
-    return { status: "OK", data: all_questions };
+    return {
+      status: "OK",
+      data: all_questions
+    };
   }
 
+  /**
+   * Format a question into the one specified in the doc
+   * @param {Object} format_question 
+   */
   async question_to_api_format(format_question) {
     const user = await UserModel.findOne({
       username: format_question.username
     });
-    if (!user)
+    if (!user) {
       return {
         status: "error",
         data: "User who created question does not exist"
       };
+    }
     const view_count = await ViewQuestionModel.countDocuments({
       question_id: format_question.id
     });
@@ -159,6 +240,9 @@ module.exports = class QuestionRepository {
       tags: format_question.tags,
       accepted_answer_id: format_question.accepted_answer_id
     };
-    return { status: "OK", data: question };
+    return {
+      status: "OK",
+      data: question
+    };
   }
 };
