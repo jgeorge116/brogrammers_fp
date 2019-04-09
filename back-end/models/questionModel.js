c5eb51e6358f65613278ad0344b3ca7ca40a5ddf
Jackie Quach <jackiequach14@gmail.com>
2019-04-08 21:20:33 -0400

    Removed autoindex from schema

--- back-end/models/questionModel.js
+++ back-end/models/questionModel.js
@@ -11,8 +11,9 @@ var QuestionModelSchema = new Schema(
     media: { type: [String], default: [] },
     accepted_answer_id: { type: String, default: null },
     timestamp: Number
-  }
+  }, {autoIndex: false}
 );
 QuestionModelSchema.set("collection", "questions");
 QuestionModelSchema.index({title:'text',body:'text'});
+await QuestionModelSchema.createIndexes();
 module.exports = mongoose.model("QuestionModel", QuestionModelSchema);
