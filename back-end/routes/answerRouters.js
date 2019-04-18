d00128505680adcd5f9a3197da816e9480edac7d
swuleung <sammiwuleung@gmail.com>
2019-04-18 13:43:35 -0400

    Make answerRouter match answerController

--- back-end/routes/answerRouters.js
+++ back-end/routes/answerRouters.js
@@ -4,8 +4,8 @@ const router = express.Router();
 var AnswerController = require("../controllers/answerController");
 
 router.post("/questions/:id/answers/add", AnswerController.add_answer);
-router.get("/questions/:id/answers", AnswerController.get_answers);
-router.get("/user/:id/answers", AnswerController.getUserAnswers);
+router.get("/questions/:id/answers", AnswerController.get_question_answers);
+router.get("/user/:id/answers", AnswerController.get_user_answers);
 router.post("/answers/:id/upvote", AnswerController.upvote_answer);
 router.post("/answers/:id/accept", AnswerController.accept_answer);
 
