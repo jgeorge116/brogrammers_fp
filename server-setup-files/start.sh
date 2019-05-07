4460cfa36dd1965709ba1a44308b13e3059b4eca
Sammi Wu Leung <sammiwuleung@gmail.com>
2019-05-07 16:00:39 -0400

    Update start.sh

--- server-setup-files/start.sh
+++ server-setup-files/start.sh
@@ -4,7 +4,3 @@ sudo apt install -y ansible
 sudo apt install -y mongodb
 sudo cp ~/brogrammers_fp/server-setup-files/hosts /etc/
 ansible-playbook launch-backend.yml
-sudo cp ~/brogrammers_fp/server-setup-files/brogrammers_nginx /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/brogrammers_nginx /etc/nginx/sites-enabled
-sudo rm /etc/nginx/sites-enabled/default
-sudo systemctl restart nginx
