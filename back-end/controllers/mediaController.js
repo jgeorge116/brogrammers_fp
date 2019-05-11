7fb901e3e04c9c4d2f45e110b7259e9478c368ef
Sammi Wu Leung <sammi.wuleung@stonybrook.edu>
2019-05-11 19:19:07 -0400

    Update mediaController.js

--- back-end/controllers/mediaController.js
+++ back-end/controllers/mediaController.js
@@ -14,7 +14,7 @@ const client = new cassandra.Client({
   contactPoints: ["192.168.122.50", "192.168.122.49"],
   // contactPoints: ["127.0.0.1"],
   localDataCenter: "datacenter1",
-  socketOptions.readTimeout: 0
+  readTimeout: 0
 });
 
 exports.add_media = async function(req, res) {
